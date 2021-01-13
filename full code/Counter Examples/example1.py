27fd4a3852bde6e3da4647239f9189f8a7edff76
tepumasuta <thinkingocean2@gmail.com>
2021-01-14 00:12:17 +0300

    Upgraded Counter example
    Refactored text parser with regex

--- full code/Counter Examples/example1.py
+++ full code/Counter Examples/example1.py
@@ -1,5 +1,6 @@
+import re
 from collections import Counter
-text = r"What is Lorem Ipsum? \
+raw_text = r"What is Lorem Ipsum? \
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \
 \
 Why do we use it? \
@@ -12,24 +13,19 @@ Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots 
 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. \
 Where can I get some?\
 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-for old_char in '.', ',', '!', '?', '"', "'":
-	text = text.replace(old_char + " ", ' ')
-for old_char in ' "', '\n', " '":
-	text = text.replace(old_char, ' ')
 
-words = Counter(map(lambda x: x.lower(), text.split(' ')))
+text = re.findall(r'[0-9a-zA-Z]+', raw_text)
+
+words = Counter(map(lambda x: x.lower(), text))
 
 print(words.most_common(5))
 
 
 # Без Counter
 
-for old_char in '.', ',', '!', '?', '"', "'":
-	text = text.replace(old_char + " ", ' ')
-for old_char in ' "', '\n', " '":
-	text = text.replace(old_char, ' ')
+text = re.findall(r'[0-9a-zA-Z]+', raw_text)
 
-words = list(map(lambda x: x.lower(), text.split(' ')))
+words = list(map(lambda x: x.lower(), text))
 just_words = set(words)
 count_word = []
 
