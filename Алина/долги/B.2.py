efdcfbdb6e604c321a9c4e292987b37c321aec59
akoretskayaa <koretskajaae05@gmail.com>
2021-02-03 21:01:23 +0300

    Added debts

---
+++ Алина/долги/B.2.py
@@ -0,0 +1,8 @@
+num1 = int(input())
+num2 = int(input()) 
+while num2 != 0:
+      mod1 = num1 % num2
+      num1 = num2
+      num2 = mod1
+print(num1)
+      
---
+++ Алина/долги/C.2.py
@@ -0,0 +1,9 @@
+num = int(input())
+am = 0
+for i in range(1, num + 1):
+      if num % i == 0:
+            am += 1
+if am == 1 or am == 2:
+      print("простое")
+else:
+      print("составное")
